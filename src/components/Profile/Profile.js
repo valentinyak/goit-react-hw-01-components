3cea735fdb46ed90d55f491eba4488fcf8f5331b
valentinyak <63512456+valentinyak@users.noreply.github.com>
2020-11-26 23:23:45 +0200

    First part of HomeWork

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types';
+import s from './Profile.module.css';
+
+const Profile = ({ name, tag, location, avatar, stats }) => {
+  return (
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img
+          src={avatar}
+          width="256px"
+          alt="Аватар пользователя"
+          className="avatar"
+        />
+        <p className="name">{name}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className={s.stats}>
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
+
+// Painting.propTypes = {
+//   imageUrl: PropTypes.string.isRequired,
+//   title: PropTypes.string.isRequired,
+//   profileUrl: PropTypes.string.isRequired,
+//   author: PropTypes.string.isRequired,
+//   price: PropTypes.number.isRequired,
+//   quantity: PropTypes.number.isRequired,
+// };
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
+};
